c66b5ae6d916a41cc067a9ae822eb02a094e7c5d
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-11 08:33:56 -0400

    EDIT index.html added some buttons to clear the grid and change the grid size EDIT style.css styled the buttons and gave the website some touchup EDIT app.js now on hover div gets filled

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const container = document.querySelector(".main-container");
 let innerHTMLString = '';
-for(let i = 1; i <= 16 ; i++) {
+let gridSize = 16;
+let color = "red";
+
+
+for(let i = 1; i <= gridSize ; i++) {
      innerHTMLString += '<div class="row"> </div>'
 }
 container.innerHTML = innerHTMLString;
@@ -9,9 +13,15 @@ const row = document.querySelectorAll(".row")
 let columnHTMLString = "";
 
 row.forEach(element => {
-    for(let j=1 ; j<= 16; j++) {
-        columnHTMLString += '<div class="box">hello</div>'
+    for(let j=1 ; j<= gridSize; j++) {
+        columnHTMLString += '<div class="box"></div>'
     }
     element.innerHTML = columnHTMLString;
     columnHTMLString = "";
 })
+
+const box = document.querySelectorAll(".box");
+
+box.forEach(draw => draw.addEventListener("mousemove" ,  function() {
+    draw.style.background = "red";
+}))
